ab5e86e7a13a396aa0af08605208ff0f48ec08b1
Murat YILDIZ <muratylz351@gmail.com>
2022-03-19 03:39:21 +0300

    düzenleme sağlandı

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <modals>
+    <template #title>
+      <h3>Ben bir Başlığıms</h3>
+    </template>
+    <template v-slot:content>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        soluta, a eius quidem temporibus optio rem sit?
+      </p>
+    </template>
+  </modals>
+</template>
+
+<script>
+import modals from "@/components/modals";
+export default {
+  components: {
+    modals,
+  },
+};
+</script>
---
+++ src/components/modals.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="header">
+<slot name="title" />
+
+  </div>
+  <div id="content">
+   <slot name="content" />
+  </div>
+  <button>Gönder</button>
+</template>
